44ec5f4c92f872589736ae22153aece2f8aea0ed
Simon Schubert <2@0x2c.org>
2015-02-03 12:15:12 +0100

    split pin change sources into MD and MI

---
+++ src/soc/kinetis/MKL26Z4/pin-change.c
@@ -0,0 +1,40 @@
+#include "../pin-change.c"
+
+extern const struct pin_change_handler pin_hooks_A, pin_hooks_A_end;
+extern const struct pin_change_handler pin_hooks_C, pin_hooks_C_end;
+extern const struct pin_change_handler pin_hooks_D, pin_hooks_D_end;
+
+void
+PORTA_Handler(void)
+{
+        pin_change_handler(PORTA_BASE_PTR, &pin_hooks_A, &pin_hooks_A_end);
+}
+
+void
+PORTD_Handler(void)
+{
+        pin_change_handler(PORTC_BASE_PTR, &pin_hooks_C, &pin_hooks_C_end);
+        pin_change_handler(PORTD_BASE_PTR, &pin_hooks_D, &pin_hooks_D_end);
+}
+
+void
+pin_change_init(void)
+{
+        if (PIN_CHANGE_ENABLE(A)) {
+                bf_set(SIM_SCGC5, SIM_SCGC5_PORTA, 1);
+                PIN_CHANGE_SET_IRQC(A);
+                int_enable(IRQ_PORTA);
+        }
+
+        if (PIN_CHANGE_ENABLE(C)) {
+                bf_set(SIM_SCGC5, SIM_SCGC5_PORTC, 1);
+                PIN_CHANGE_SET_IRQC(C);
+        }
+        if (PIN_CHANGE_ENABLE(D)) {
+                bf_set(SIM_SCGC5, SIM_SCGC5_PORTD, 1);
+                PIN_CHANGE_SET_IRQC(D);
+        }
+        if (PIN_CHANGE_ENABLE(C) || PIN_CHANGE_ENABLE(D)) {
+                int_enable(IRQ_PORTD);
+        }
+}
--- src/soc/kinetis/pin_change.c
+++ src/soc/kinetis/pin_change.c
@@ -15,35 +15,14 @@ pin_change_handler(PORT_MemMapPtr port,
         }
 }
 
-#define PORT_CHANGE_HANDLER(port) \
-        extern const struct pin_change_handler pin_hooks_##port, pin_hooks_##port##_end; \
-        \
-        void \
-        PORT##port##_Handler(void) \
-        { \
-                pin_change_handler(PORT##port##_BASE_PTR, &pin_hooks_##port, \
-                                   &pin_hooks_##port##_end);       \
-        }
+#define FOREACH_PIN_CHANGE(port) \
+        for (const struct pin_change_handler *i = &pin_hooks_##port; i < &pin_hooks_##port##_end; i++)
 
-PORT_CHANGE_HANDLER(A);
-PORT_CHANGE_HANDLER(B);
-PORT_CHANGE_HANDLER(C);
-PORT_CHANGE_HANDLER(D);
+#define PIN_CHANGE_ENABLE(port) \
+        (&pin_hooks_##port != &pin_hooks_##port##_end)
 
-#define pin_change_init_port(port)                                      \
-        for (const struct pin_change_handler *i = &pin_hooks_##port;    \
-             i < &pin_hooks_##port##_end; i++) {                        \
-                bf_set(SIM_SCGC5, SIM_SCGC5_PORT##port, 1);             \
-                PORT_MemMapPtr p = PORT##port##_BASE_PTR;               \
+#define PIN_CHANGE_SET_IRQC(port)                                         \
+        FOREACH_PIN_CHANGE(port) {                                        \
+                PORT_MemMapPtr p = PORTA_BASE_PTR;                      \
                 bf_set(PORT_PCR_REG(p, pin_physpin_from_pin(i->pin_id)), PORT_PCR_IRQC, i->polarity); \
-                int_enable(IRQ_PORT##port);                             \
         }
-
-void
-pin_change_init(void)
-{
-        pin_change_init_port(A);
-        pin_change_init_port(B);
-        pin_change_init_port(C);
-        pin_change_init_port(D);
-}
