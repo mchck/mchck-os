f3f1bcd20f294caef52b7afecc5d1e7b1dae89c0
Simon Schubert <2@0x2c.org>
2015-10-14 17:28:31 +0800

    board/current-monitor: support multiple onboard LEDs

--- src/board/current-monitor/board.c
+++ src/board/current-monitor/board.c
@@ -1,10 +1,12 @@
 #include <mchck.h>
 
-static const enum gpio_pin_id led_pin = GPIO_PTC8;
+static const enum gpio_pin_id led_pins[] = {GPIO_PTC8, GPIO_PTC9, GPIO_PTC10, GPIO_PTC11};
 
 void
 onboard_led(enum onboard_led_state state)
 {
+        enum gpio_pin_id led_pin = led_pins[(state & ONBOARD_LED_MASK) >> 8];
+
         if (state == ONBOARD_LED_FLOAT) {
                 gpio_dir(led_pin, GPIO_DISABLE);
                 return;
--- src/board/current-monitor/board.h
+++ src/board/current-monitor/board.h
@@ -1,6 +1,11 @@
 enum onboard_led_state {
         ONBOARD_LED_OFF = 0,
         ONBOARD_LED_ON = 1,
+        ONBOARD_LED_MASK = 0xf00,
+        ONBOARD_LED_ERROR = 0x000,
+        ONBOARD_LED_CALIBRATE = 0x100,
+        ONBOARD_LED_CAPTURE = 0x200,
+        ONBOARD_LED_POWER = 0x300,
         ONBOARD_LED_TOGGLE = -1,
         ONBOARD_LED_FLOAT = -2
 };
