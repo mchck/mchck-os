ae9639170ec37cd3686b80ca61c4f4126cbe2a25
Simon Schubert <2@0x2c.org>
2016-01-11 00:49:49 +0100

    sema: add SEMA_INI

--- src/common/sema.h
+++ src/common/sema.h
@@ -2,6 +2,8 @@ struct sema {
         uint32_t count;
 };
 
+#define SEMA_INIT {0}
+
 void sema_wait_count(struct sema *sema, int count);
 void sema_wait(struct sema *sema);
 void sema_wake_count(struct sema *sema, int count);
